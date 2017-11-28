1b05b6c6b7f826d2c7ffaa38efe51b89ada6c3ec
Unknown <a.searle274@gmail.com>
2017-11-28 09:53:43 -0600

    key, plaintext, cyphertext arrays

--- main.asm
+++ main.asm
@@ -9,10 +9,15 @@
 .stack 4096 
 ExitProcess PROTO, dwExitCode:DWORD 
 .data 
- 
+	pt byte	"ASH"							; plain text array
+	key byte ?, lengthof pt - 1				; key array (one less length of pt)
+	ct byte lengthof pt DUP(?)				; cyphertext
+
 .code 
   main PROC 
-								 
+	mov eax, 0
+	
+						
     invoke ExitProcess, 0 
   main endp 
 end main
